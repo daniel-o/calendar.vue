2eb6209f3e554c9182b93d29a0cb4bcfb568c844
Daniel Owens <danielodev@gmail.com>
2017-04-03 13:24:02 -0500

    Added a context describe for the `addEvent` function to the tests.

--- test/calendar.test.js
+++ test/calendar.test.js
@@ -1,19 +1,21 @@
 const Calendar = require( "../src/calendar.js" );
 
 describe( "Calendar", function() {
-	it( "will add an event", function() {
-		const date = new Date( 2017, 3, 5, 9, 30 );
-		const cal = new Calendar();
-		cal.addEvent( date, { one: 1 } );
+	describe( "addEvent", function() {
+		it( "will construct the date and time buckets", () => {
+			const date = new Date( 2017, 3, 5, 9, 30 );
+			const cal = new Calendar();
+			cal.addEvent( date, { one: 1 } );
 
-		// NOTE: The date will be converted to UTC timezone after being passed
-		// to the Calendar object.
-		const expected = {
-			"4/5/2017" : {
-				"14:30:00" : [ { one: 1 } ]
-			}
-		};
+			// NOTE: The date will be converted to UTC timezone after being passed
+			// to the Calendar object.
+			const expected = {
+				"4/5/2017" : {
+					"14:30:00" : [ { one: 1 } ]
+				}
+			};
 
-		expect( cal.events ).toEqual( expected );
+			expect( cal.events ).toEqual( expected );
+		} );
 	} );
 } );
