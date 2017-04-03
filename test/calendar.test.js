9c6439dff7a5efc588153493d3feb5aa3f3cdb06
Daniel Owens <danielodev@gmail.com>
2017-04-03 15:55:23 -0500

    Wrote test for adding multiple date events.

--- test/calendar.test.js
+++ test/calendar.test.js
@@ -1,5 +1,10 @@
 const Calendar = require( "../src/calendar.js" );
 
+/*
+ * NOTE: When constructing a Date object all month values are 0 based.
+ * So when comparing output values increment the number by one.
+ */
+
 describe( "Calendar", function() {
 	describe( "addEvent", function() {
 		it( "will construct the date and time buckets", () => {
@@ -29,6 +34,32 @@ describe( "Calendar", function() {
 			const expected = {
 				"4/5/2017" : {
 					"14:30:00" : [ { one: 1 }, { two: 2 } ]
+				}
+			};
+
+			expect( cal.events ).toEqual( expected );
+		} );
+
+
+		it( "will create multiple date and time buckets", () => {
+			const one = new Date( 2017, 3, 5, 9, 30 );
+			const two = new Date( 2017, 3, 5, 4, 15 );
+			const three = new Date( 2019, 6, 6, 6, 00 );
+
+			const cal = new Calendar();
+			cal.addEvent( one, { one: 1 } );
+			cal.addEvent( two, { two : 2 } );
+			cal.addEvent( three, { three: 3 } );
+
+			// NOTE: The date will be converted to UTC timezone after being passed
+			// to the Calendar object.
+			const expected = {
+				"4/5/2017" : {
+					"14:30:00" : [ { one: 1 } ],
+					"09:15:00" : [ { two: 2 } ]
+				},
+				"7/6/2019" : {
+					"11:00:00" : [ { three: 3 } ]
 				}
 			};
 
