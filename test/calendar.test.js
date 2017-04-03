248ee7aaa6c0922e4fef5b6ee4f9009e1c95e714
Daniel Owens <danielodev@gmail.com>
2017-04-03 13:13:37 -0500

    Wrote test for adding an event object.

--- test/calendar.test.js
+++ test/calendar.test.js
@@ -1,4 +1,19 @@
+const Calendar = require( "../src/calendar.js" );
+
 describe( "Calendar", function() {
-	it( "test", function() {
+	it( "will add an event", function() {
+		// NOTE: The date will be converted to UTC timezone after being passed
+		// to the Calendar object.
+		const date = new Date( 2017, 3, 5, 9, 30 );
+		const cal = new Calendar();
+		cal.addEvent( date, { one: 1 } );
+
+		const expected = {
+			"4/5/2017" : {
+				"14:30:00" : [ { one: 1 } ]
+			}
+		};
+
+		expect( cal.events ).toEqual( expected );
 	} );
 } );
