d3a7f465dd7ffc0e78de1bfab896f4a4faddaa88
Daniel Owens <danielodev@gmail.com>
2017-04-03 15:54:07 -0500

    Added webpack task that loads and compiles vue files.
    Also unifies library into single file 'calendar.vue.js' file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 const gulp = require( "gulp" );
 const jasmine = require( "gulp-jasmine" );
+const webpack = require( "webpack-stream" );
+const vueLoader = require( "vue-loader" );
+
+gulp.task( "webpack", () =>
+		gulp.src( "src/*.vue" )
+			.pipe( webpack( {
+				output: { filename: "calendar.vue.js" },
+
+				module: { loaders: [ {
+					test: /vue$/,
+					loader: "vue-loader"
+				} ] }
+			} ) )
+
+			.pipe( gulp.dest( "dist/" ) )
+);
 
 gulp.task( "test", () =>
 	gulp.src( "test/**/*.test.js" ).pipe( jasmine() )
