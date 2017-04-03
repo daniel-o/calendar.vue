41f48e383419b78e41ad3e9bbccfedd8bc8da950
Daniel Owens <danielodev@gmail.com>
2017-04-03 13:27:41 -0500

    Wrote a test for making sure multiple events may be added to a single date and time.

--- test/calendar.test.js
+++ test/calendar.test.js
@@ -17,5 +17,22 @@ describe( "Calendar", function() {
 
 			expect( cal.events ).toEqual( expected );
 		} );
+
+		it( "will append and event to previously added time buckets", () => {
+			const date = new Date( 2017, 3, 5, 9, 30 );
+			const cal = new Calendar();
+			cal.addEvent( date, { one: 1 } );
+			cal.addEvent( date, { two : 2 } );
+
+			// NOTE: The date will be converted to UTC timezone after being passed
+			// to the Calendar object.
+			const expected = {
+				"4/5/2017" : {
+					"14:30:00" : [ { one: 1 }, { two: 2 } ]
+				}
+			};
+
+			expect( cal.events ).toEqual( expected );
+		} );
 	} );
 } );
