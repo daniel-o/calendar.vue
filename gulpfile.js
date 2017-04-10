b10c506bad550e19e78d64ddaa07ad90107e3f07
Daniel Owens <danielodev@gmail.com>
2017-04-10 13:21:48 -0500

    Changed back to single entry point and output file.

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,18 @@ const jasmine = require( "gulp-jasmine" );
 const webpack = require( "webpack-stream" );
 const vueLoader = require( "vue-loader" );
 
-gulp.task( "webpack", () =>
-		gulp.src( "src/*.vue" )
+gulp.task( "dist", () =>
+		gulp.src( "src/calendar.vue.js" )
 			.pipe( webpack( {
-				output: { filename: "calendar.vue.js" },
-
 				module: { loaders: [ {
 					test: /vue$/,
 					loader: "vue-loader"
-				} ] }
+				} ] },
+
+				output: {
+					filename: "calendar.vue.js",
+					umdNamedDefine: false
+				}
 			} ) )
 
 			.pipe( gulp.dest( "dist/" ) )
