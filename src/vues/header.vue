a426370c5369fffb6192ed0f5988fb127846e0ee
Daniel Owens <danielodev@gmail.com>
2017-04-12 14:41:18 -0500

    Changed header template to be constructed of smaller components.

--- src/vues/header.vue
+++ src/vues/header.vue
@@ -1,7 +1,5 @@
 <template id="toolbar-vue">
-<header>
-	<span class="cal-month">{{ monthName }}</span>
-	<span class="dates">{{ dateRange }}</span>,
-	<span class="year">{{ new Date( this.selectedDate || Date.now() ).getFullYear() }}</span>
+<header >
+	<dateRange :start="startWeekDate()" :end="endWeekDate()"></dateRange>
 </header>
 </template>
