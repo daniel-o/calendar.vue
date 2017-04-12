22a8c6469163d808d4998da675c5b3e1fe031c0e
Daniel Owens <danielodev@gmail.com>
2017-04-12 14:42:53 -0500

    Now includes DateLabel component.
    Wrote functions for computing the week's start and end dates.

--- src/header.js
+++ src/header.js
@@ -1,25 +1,39 @@
 const HeaderTemplate = require( "./vues/header.vue" );
+const DateLabel = require( "./date-range.vue.js" ).RangeLabel;
 
 exports.Header = Vue.extend( {
 	mixins: [ HeaderTemplate ],
-	props: [ "selectedDate" ],
-	computed: {
-		monthName: function() {
-			return new Date( this.selectedDate || Date.now() )
-				.toLocaleString( "en-us", { month: "long" } );
-		},
-		dateRange: function() {
-			// daysInWeek is a base 0 range.
-			const daysInWeek = 6;
-			const date = new Date( this.selectedDate || Date.now() );
-			const current = date.getDate();
-			const day = date.getDay();
+	props: [ "date" ],
 
-			const start = current - day;
-			console.log( start );
-			const end = daysInWeek - day + current;
-			
-			return start + " - " + end;
+	components: {
+		dateRange: DateLabel
+	},
+
+	methods: {
+		startWeekDate: function() {
+			const day = this.currentDate.getDate() - this.currentDate.getDay();
+			console.log( this.currentDate );
+			console.log( this.currentDate.setDate( day ) );
+			return this.currentDate.setDate( day );
+		},
+
+		endWeekDate: function() {
+			const day = 6 - this.currentDate.getDay() + this.currentDate.getDate();
+			return this.currentDate.setDate( day );
+		},
+
+		monthLength: function() {
+			return new Date( this.currentDate.setDate( 0 ) ).getDate();
+		},
+
+		splitMonth: function() {
+			return ( this.rawStartWeekDate() < 0 ) || ( this.rawEndWeekDate() > this.monthLength() );
+		}
+	},
+
+	computed: {
+		currentDate: function() {
+			return new Date( this.date );
 		}
 	}
 } );
