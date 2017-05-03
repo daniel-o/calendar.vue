718a02f4476da192f356713e02b4b980f2c96d0d
Daniel Owens <danielodev@gmail.com>
2017-05-03 14:18:39 -0500

    Added generic template for displaying month.

---
+++ src/month.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="cal-month">
+		<span class="cal-date" v-for="date in dateRange">{{ getDay( date ).getDate() }},</span>
+	</div>
+</template>
+
+<script>
+	module.exports = {
+		props: [ "date" ],
+
+		computed: {
+			currentMonth: function() {
+				return this.date.getMonth();
+			}
+		}
+	};
+</script>
