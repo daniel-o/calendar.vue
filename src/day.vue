6426180a4516b9f6f42b18fe1554dc7afaf21473
Daniel Owens <danielodev@gmail.com>
2017-08-27 18:48:41 -0500

    Added clickable day link.

--- src/day.vue
+++ src/day.vue
@@ -1,11 +1,17 @@
 <template>
 	<div class="cal-day">
-		{{ this.date.getDate() }}
+		<a v-on:click="selectDate">{{ this.date.getDate() }}</a>
 	</div>
 </template>
 
 <script>
 	module.exports = {
 		props: [ "date", "state" ]
+	},
+
+	methods: {
+		selectDate: function() {
+			this.$emit( "change" );
+		}
 	}
 </script>
