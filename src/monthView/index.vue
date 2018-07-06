7df4d17a21f405e878d962e3067aeb54f19ec816
Daniel Owens <danielodev@gmail.com>
2018-07-06 14:51:42 -0500

    Added Day component.

--- src/monthView/index.vue
+++ src/monthView/index.vue
@@ -1,16 +1,20 @@
 <template>
 	<month :date="date">
-		<div slot-scope="props">{{ new Date( props.date ).getDate() }}</div>
+		<div slot-scope="props">
+			<day :date="props.date" :selectedDate="date"></day>
+		</div>
 	</month>
 </template>
 
 <script>
 	import Month from "../month.vue";
+	import Day from "../day.vue";
 
 	export default {
 		props: [ "date" ],
 
 		components: {
+			Day,
 			Month
 		}
 	}
