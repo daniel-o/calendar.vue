a6b1149e6cdcdc40661d2973a82f7edd6a3af871
Daniel Owens <danielodev@gmail.com>
2018-07-06 12:56:50 -0500

    Spiced up the stylesheet to handle whitespace a little better.

--- src/day.vue
+++ src/day.vue
@@ -6,7 +6,16 @@
 
 <style>
 	.cal-day {
-		padding: 0.5em;
+		width: 1em;
+		height: 1em;
+		padding: 0.2em;
+		margin: 0.2em;
+		border-radius: 9999px;
+		text-align: center;
+	}
+
+	.cal-day.cal-current-date {
+		background-color: #dbdde0;
 	}
 </style>
 
