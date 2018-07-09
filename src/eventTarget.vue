a60b7ccdb01b0773497cebd81c16f2fb7835ca02
Daniel Owens <danielodev@gmail.com>
2018-07-09 02:06:41 -0500

    Created EventTarget component that uses EventTarget mixin.

---
+++ src/eventTarget.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="cal-event-target" @click="createEvent()">
+	</div>
+</template>
+
+<style>
+	.cal-event-target {
+		flex-grow: 1;
+	}
+</style>
+
+<script>
+	import EventTarget from "./eventTarget.mixin.js";
+
+	export default {
+		mixins: [ EventTarget ]
+	}
+</script>
