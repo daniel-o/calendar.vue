055fab46182a15bcf8e8a15cd7a13b6cb1dcbea9
Daniel Owens <danielodev@gmail.com>
2018-07-09 10:32:41 -0500

    Created generic div for displaying events.

--- src/monthView/index.vue
+++ src/monthView/index.vue
@@ -2,6 +2,10 @@
 	<month :date="date" :events="events">
 		<div class="cal-column" slot-scope="props">
 			<day :date="props.date" :selectedDate="date"></day>
+			<div
+				class="cal-event-tag"
+				v-for="event in props.events"
+				:title="event.title">{{ event.title }}</div>
 			<event-target :start="props.date" v-on="$listeners"></event-target>
 		</div>
 	</month>
@@ -9,10 +13,25 @@
 
 <style>
 	.cal-column {
+		flex-grow: 1;
 		height: 100%;
 
 		display: flex;
 		flex-direction: column;
+	}
+
+	.cal-event-tag {
+		font-size: 0.8em;
+		box-sizing: border-box;
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+
+		padding: 0.2em;
+		margin: 0.2em;
+		background-color: #a3bbe2;
+		border-radius: 3px;
+		cursor: default;
 	}
 </style>
 
