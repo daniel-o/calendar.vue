11778c4bcf9cc44d9b2d44a4f64a335e11195188
Daniel Owens <danielodev@gmail.com>
2018-07-09 01:50:17 -0500

    Added EventTarget mixin.
    Triggers createEvent when target area clicked.
    
    Styled it up a bit.

--- src/monthView/index.vue
+++ src/monthView/index.vue
@@ -1,17 +1,33 @@
 <template>
 	<month :date="date">
-		<div slot-scope="props">
+		<div class="cal-column" slot-scope="props">
 			<day :date="props.date" :selectedDate="date"></day>
+			<div class="cal-event-target" @click="createEvent()"></div>
 		</div>
 	</month>
 </template>
 
+<style>
+	.cal-column {
+		height: 100%;
+
+		display: flex;
+		flex-direction: column;
+	}
+
+	.cal-event-target {
+		grow: 1;
+	}
+</style>
+
 <script>
 	import Month from "../month.vue";
 	import Day from "../day.vue";
+	import EventTarget from "../eventTarget.mixin.js";
 
 	export default {
 		props: [ "date" ],
+		mixins: [ EventTarget ],
 
 		components: {
 			Day,
